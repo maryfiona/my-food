96c77336e0c1fae781d18da4db5a95897469daee
agu chidimma maryfiona <aguchidimmamaryfiona@gmail.com>
2023-08-01 19:43:35 +0100

    happy new month

--- src/Section2/Inspire.js
+++ src/Section2/Inspire.js
@@ -5,20 +5,20 @@ import image1 from '../Assets/image1.png'
 const Inspire = () => {
     return(
         <React.Fragment>
-            <div className="na pt">
+            {/* <div className="na pt">
                 <h1 className="our text-center">Inspiration For your First Order</h1>
                 <div className="container pt-5">
-                    <div className="row">
+                    <div className="row"> */}
                         <div className="col-md-3">
                         <button className="bo">
                         <img src={image1} alt="img1" />
                         <p className="our">Locals</p>
                         </button>
 
-                        </div>
+                        </div> 
 
 
-                        
+
                         <div className="col-md-3">
 
                         </div>
